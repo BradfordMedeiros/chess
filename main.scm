f5e5f97bf88d2eed903fe38ff13e320dcd6e9843
brad medeiros <bradmedeiros0@gmail.com>
2019-09-15 21:27:35 -0700

    chess basically implemented, now going to integrate in engine

--- main.scm
+++ main.scm
@@ -1,7 +1,7 @@
-
 (use-modules (srfi srfi-43))
 (use-modules (ice-9 readline))
 (use-modules (ice-9 match))
+(use-modules (ice-9 eval-string))
 
 (define board 
   #(
@@ -49,6 +49,9 @@
   #(2 0 0 0 2)
   #(0 2 0 2 0)
 ))
+(define no-moves #(
+  #(x)
+))
 
 (define symbol-to-moves '(
   ("pw"   pawn-white-moves  ) 
@@ -63,83 +66,118 @@
   ("bb"   bishop-moves      )
   ("hw"   horse-moves       )
   ("hb"   horse-moves       )
+  (0      no-moves          )
 ))
 
-
-
-
-(define (merge-possible-moves current-board piece-moves xPos yPos)
-   (display piece-moves)
+(define (get-moves-center piece-moves)
+  (define center (cons -1 -1))
+  (do ((row 0 (+ row 1))) ((>= row (vector-length piece-moves)))
+    (let ((current-row (vector-ref piece-moves row)))
+      (do ((col 0 (+ col 1))) ((>= col (vector-length current-row)))
+        (if (equal? 'x (vector-ref current-row col)) 
+          (set! center (cons row col))
+        )
+      )
+    )
+  )
+  center
 )
+
+(define (get-value-from-moves piece-moves row col yPos xPos defaultValue)
+  (define centerMoves (get-moves-center piece-moves))
+  (let ((relativeY (- row (- yPos (cdr centerMoves)))) (relativeX (- col (- xPos (car centerMoves)))) (movesHeight (vector-length piece-moves)) (movesWidth (vector-length (vector-ref piece-moves 0))))
+    (cond
+      ((< relativeX 0) defaultValue)
+      ((< relativeY 0) defaultValue)
+      ((>= relativeY movesHeight) defaultValue)
+      ((>= relativeX movesWidth) defaultValue)
+      (#t (vector-ref (vector-ref piece-moves relativeY) relativeX))
+    )
+  )
+)
+
+(define (merge-possible-moves current-board piece-moves yPos xPos)
+  (define newboard #())
+  (define pieceHeight (vector-length piece-moves))
+  (define pieceWidth  (vector-length (vector-ref piece-moves 0)))
+  (do ((row 0 (+ row 1))(newRow #())) ((>= row (vector-length current-board)))
+    (let ((current-row (vector-ref current-board row)))
+      (define newRow #())
+      (do ((col 0 (+ col 1))) ((>= col (vector-length current-row)))
+        (set! newRow (vector-append newRow (make-vector 1 (get-value-from-moves piece-moves row col yPos xPos (vector-ref current-row col)))))
+      )
+      (set! newboard (vector-append newboard (make-vector 1 newRow)))
+    )
+  )
+  newboard
+)
+
+(define (piece-at-index board row col) (vector-ref (vector-ref board row) col))
+(define (get-moves-for-piece piece) (cadr (assoc piece symbol-to-moves)))
+(define (moves-at-index board row col) (get-moves-for-piece (piece-at-index board row col)))
+
 (define (remaining-pieces board color)
-  (define x 0)
+  (define numPieces 0)
   (do ((i 0 (+ i 1))) ((>= i (vector-length board)))
     (let ((vector-row (vector-ref board i)))
       (do ((j 0 (+ j 1))) ((>= j (vector-length vector-row) ))
         (let ((value (vector-ref vector-row j)))
           (cond 
             ((equal? value 0)(display ""))
-            ((equal? (substring value 1 2) color) (set! x (+ x 1)))
+            ((equal? (substring value 1 2) color) (set! numPieces (+ numPieces 1)))
             (#t (+ 0 0))
           )
         )
       )  
     )
   )
-  x
+  numPieces
 )
 
-(define value (remaining-pieces board "b"))
-(display "value is: ")
-(display value)
-(display "\n")
+(define (moveboard fromXPos fromYPos toXPos toYPos)
+  board
+)
 
 (define (printboard board index) 
   (let ((length (vector-length board)))
     (if (< index length) 
         (begin
-          (display index)
-          (display " ")
-          (display (vector-ref board index))
-          (display "\n")
+          (display index)(display " ")(display (vector-ref board index))(display "\n")
           (printboard board (+ index 1))
         ) 
     )
   )
 )
 
-(define (printinfo board) 
-  (display "Current turn: ")
-  (if is-white-turn (display "white") (display "black"))
-  (display "\n\n")
-  (printboard board 0)
-)
-
-(define is-white-turn #t)
-
 (define (processGameMove move)
-  ;(set! is-white-turn (not is-white-turn))
   (match (string-split move #\ )
-    (("move" xPos yPos . rest) (begin 
-      (display "xPos is: ")
-      (display xPos)
-      (display "\n")
-    ))
+    (("move" fromXPos fromYPos toXPos toYPos . rest) 
+      (moveboard fromXPos fromYPos toXPos toYPos)
+    )
     (("query" xPos yPos . rest) (begin 
-      (display "xPos is: ")
-      (display xPos)
-      (display "\n")
-      (merge-possible-moves board pawn-white-moves xPos yPos)
+      (define xPosInt (string->number xPos))
+      (define yPosInt (string->number yPos))
+      (define moves (moves-at-index board xPosInt yPosInt))
+      (define moveSet (eval moves (interaction-environment)))
+      (merge-possible-moves board moveSet xPosInt yPosInt)
     ))
-    (default (display "invalid query"))
+    (default "invalid query")
   )
 )
 
 (define (inputToAction input)
   (cond 
     ((string=? input "info") (printinfo board))
-    (else (processGameMove input))
+    (else (printboard (processGameMove input) 0))
   )
+)
+
+(define is-white-turn #t)
+
+(define (printinfo board) 
+  (display "Current turn: ")
+  (if is-white-turn (display "white\n\n") (display "black\n\n"))
+  (printboard board 0)
 )
 
 (define (gameloop)
@@ -147,4 +185,9 @@
   (gameloop)
 )
 
-(define pgm processGameMove)
+
+
+;(define newboard (merge-possible-moves board horse-moves 0 1))
+;(printinfo newboard)
+;(gameloop)
+
